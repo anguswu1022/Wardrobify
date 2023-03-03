e640c25c7126a2a307664e62423730945f5cc517
Angus Wu <anguswu1022@ucla.edu>
2023-03-03 01:36:39 +0000

    Revert "Merge branch 'angus-shoe'"
    This reverts commit 8ecbbe4d7aa1889e50e67edf148232fd602d6483

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class BinVO(models.Model):
-    import_href = models.CharField(max_length=200, unique=True)
-    closet_name = models.CharField(max_length=100)
-    bin_number = models.PositiveSmallIntegerField()
-    bin_size = models.PositiveSmallIntegerField()
-
-class Shoe(models.Model):
-    manufacturer = models.CharField(max_length=200)
-    model_name = models.CharField(max_length=200)
-    color = models.CharField(max_length=200)
-    picture_url = models.URLField(null=True)
-    bin = models.ForeignKey(
-        BinVO,
-        related_name="shoe",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-    )
--- shoes/api/shoes_rest/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import api_list_shoes
-
-urlpatterns = [
-    path('shoes', api_list_shoes, name="api_list_shoes"),
-]
