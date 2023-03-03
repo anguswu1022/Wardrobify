f0826df868e1d17854235cd9261134c5da86d566
Peter <petertrinh00@gmail.com>
2023-03-03 15:53:36 -0800

    Form and Delete done

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import HatForm from './HatForm';
+import HatList from './HatList'
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="hats">
+            <Route index element={<HatList />} />
+            <Route path="new" element={<HatForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
